c6129a43b2b6963085be4ff8195ebf3fc0696e21
Faridanwar <62336668+Faridanwar@users.noreply.github.com>
2020-04-22 09:48:01 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes{
+    public static void main (String [] args){
+        String namaDepan = "Muhammad";
+        String namaTengah = "Farid";
+        String namaBelakang = "Anwar";
+        int usia = 19;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+    }
+}
